622fe9d03514563ce7cc20b4194b0ccaf6e13d7f
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2025-06-06 09:17:04 +0000

    code

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,6 @@
+const accountid=112233;
+let accountname="Aman";
+var accountemail="aman120@gmail.com";
+accountcity="khurai";
+ let accountstate;
+console.table([accountid,accountname,accountemail,accountcity,accountstate]);
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+ "use strict";  //treat all jscode as a newer version 
+//  alert(3+3); we are using nodejs not a browser
+//  number datatypes range 2^53
+// String="aman"
+// BigInt
+// Boolean
+// null->> standalone value output- object
+// undefined output-undefined
+// Symbol->> unique
+let name="aman";
+var age=30;
+console.log(typeof age);
+console.log(typeof null);
