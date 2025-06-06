d8708df402d1cf8349e325b42bae38573f6d1eeb
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2025-06-06 16:08:27 +0000

    code

---
+++ .devcontainer/01_basics/03_conversionoperators.js
@@ -0,0 +1,15 @@
+// let score="33ab"
+
+// console.log(typeof score)
+// let valueINNumber= Number(score)
+// console.log(typeof valueINNumber)
+// console.log(valueINNumber)
+// "33"->> 33
+// "33abs"->> NaN
+// "true"->> false
+let ISLoggedIn= 1
+let booleanISLogged=1
+
+let booleanISLoggedIn= Boolean(ISLoggedIn)
+
+console.log(booleanISLogged);
