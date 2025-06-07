1b5524109ee3f1a21e362837a98f1cdf87287d9c
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2025-06-07 03:56:04 +0000

    code

--- .devcontainer/01_basics/03_conversionoperators.js
+++ .devcontainer/01_basics/03_conversionoperators.js
@@ -18,3 +18,22 @@ console.log(bool)
 // 0>>false
 // "">>false
 // "aman">>true
+
+// *********** operations on data types *********
+
+let value = 3
+ let negvalue = -value
+
+ console.log(negvalue);
+
+//  console.log(2+2);
+//  console.log(2-2);
+//   console.log(2*2);
+//   console.log(2/3);
+// //   console.log(2%3);
+//  let str1="hello"
+//  let str2=" aman"
+//  console.log(str1+str2);
+
+// console.log("1"+2);
+console.log(1+2+"3");
