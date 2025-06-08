78f02c0e262b938a19e3a6e496540692279508e0
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2025-06-08 15:08:12 +0000

    string

---
+++ .devcontainer/01_basics/04_string.js
@@ -0,0 +1,7 @@
+let name="aman"
+let age= 23
+console.log('hello my name is $: {name} and my age is $: {23}');
+
+const gamename= new String('pungentshrine12')
+console.log(gamename[0])
+console.log(gamename.__proto__);
