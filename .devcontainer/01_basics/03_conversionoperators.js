446ecde17ddd38eac94871d541c4ea852f4bbcd9
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2025-06-08 02:21:56 +0000

    code

--- .devcontainer/01_basics/03_conversionoperators.js
+++ .devcontainer/01_basics/03_conversionoperators.js
@@ -8,11 +8,11 @@
 // "33abs"->> NaN
 // "true"->> false
 
-let isloggedin = "aman"
+// let isloggedin = "aman"
 
-let bool = Boolean(isloggedin)
+// let bool = Boolean(isloggedin)
 
-console.log(bool)
+// console.log(bool)
 
 // 1>>true
 // 0>>false
@@ -21,10 +21,10 @@ console.log(bool)
 
 // *********** operations on data types *********
 
-let value = 3
- let negvalue = -value
+// let value = 3
+//  let negvalue = -value
 
- console.log(negvalue);
+ //console.log(negvalue);
 
 //  console.log(2+2);
 //  console.log(2-2);
@@ -37,11 +37,16 @@ let value = 3
 
 //  console.log(["1"]+[2]);
 //  console.log([1+2]+"3");
-let x=3
-let y=x++ // retuns value before incrementing
+// let x=3
+// let y=x++ // retuns value before incrementing
 //let y=++x  //returns value after incrementing
-console.log(x,y)
- console.log("2"<1)
- console.log(null>0)
- console.log(null>=0) //
- 
+// console.log(x,y)
+//  console.log("2"<1)
+//  console.log(null>0)
+//  console.log(null>=0) //comparision converts null to a number treating it as 0 >>true
+//  console.log(null==0) //equal are different from comparision operators >>false
+  console.log(undefined==0) //>> false
+  console.log(undefined>=0)  //>> false
+  console.log(undefined<=0)  //>> false
+  // === it chack the values and its datatypes in the conversion operations
+  console.log("2"===2);
