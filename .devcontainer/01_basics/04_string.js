a7e45b19d5105328e85750017f0abb4d73c4ac0e
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2025-06-09 14:41:54 +0000

    string method

--- .devcontainer/01_basics/04_string.js
+++ .devcontainer/01_basics/04_string.js
@@ -1,27 +1,35 @@
-let name="aman"
-let age= 23
-console.log('hello my name is $: aman and my age is $: 23');
+// let name="aman"
+// let age= 23
+// console.log('hello my name is $: aman and my age is $: 23');
 
-const gamename= new String('pungent-shrine-12')
-const gamename2=new String('online');
-console.log(gamename[0])
-console.log(gamename.__proto__);
-console.log(gamename.charAt(2));
-console.log(gamename.indexOf('s'));
-console.log(gamename.length);
-console.log(gamename.slice(6,9));
-console.log(gamename.toUpperCase());
-let user1="am"
-let user2="an"
-let user3="sin"
-let user4= 12
-let result= user1.concat("",user2,user3,user4)
-console.log(result);
-let num1= "  asma   "
-console.log(num1.trim());
-let farmerid= "44123544433221678"
-console.log(farmerid.substring(5,12))
-const url="https//amansingh%20rajpoot.com"
-console.log(url.replace('%20', '-'));
-console.log(url.includes('singh'));
-console.log(gamename.split('-'));  //spilt method converts string into array by passing separator and limit in it
+// const gamename= new String('pungent-shrine-12')
+// const gamename2=new String('online');
+// console.log(gamename[0]) // this way is used to access a character by array method
+// console.log(gamename.__proto__);
+// console.log(gamename.charAt(2)); 
+// console.log(gamename.indexOf('s'));
+// console.log(gamename.length);
+// console.log(gamename.slice(6,9));
+// console.log(gamename.toUpperCase());
+// let user1="am"
+// let user2="an"
+// let user3="sin"
+// let user4= 12
+// let result= user1.concat("",user2,user3,user4)
+// console.log(result);
+// let num1= "  asma   "
+// console.log(num1.trim());
+// let farmerid= "44123544433221678"
+// console.log(farmerid.substring(5,12))
+// const url="https//amansingh%20rajpoot.com"
+// console.log(url.replace('%20', '-'));
+// console.log(url.includes('singh'));
+// console.log(gamename.split('-'));  //spilt method converts string into array by passing separator and limit in it
+ 
+const strprim= 'pump'
+const strprim2= String(1)
+const strprim3= String(true)
+const strobj= new String(strprim)
+// console.log(strobj);
+console.log(typeof strprim)
+console.log(typeof strobj);
