e7844d0cc90704a5549bc285dd53a8ba1bd5b175
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2025-06-09 15:10:57 +0000

    number function in javascript

---
+++ .devcontainer/01_basics/05_num_and_maths.js
@@ -0,0 +1,14 @@
+const score= 100
+console.log(score);
+
+const balance= new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2)) //to fixed property is mainly used in e-commerce website
+
+const num= 30.03
+console.log(num.toPrecision(3))
+
+const hundreds= 1000000
+console.log(hundreds.toLocaleString('en-IN'));
