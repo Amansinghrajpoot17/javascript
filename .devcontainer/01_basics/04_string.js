216011a0002425f07c157865186fae0339197266
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2025-06-09 11:29:45 +0000

    method

--- .devcontainer/01_basics/04_string.js
+++ .devcontainer/01_basics/04_string.js
@@ -1,8 +1,8 @@
 let name="aman"
 let age= 23
-console.log('hello my name is $: {name} and my age is $: {23}');
+console.log('hello my name is $: aman and my age is $: 23');
 
-const gamename= new String('pungentshrine12')
+const gamename= new String('pungent-shrine-12')
 const gamename2=new String('online');
 console.log(gamename[0])
 console.log(gamename.__proto__);
@@ -23,3 +23,5 @@ let farmerid= "44123544433221678"
 console.log(farmerid.substring(5,12))
 const url="https//amansingh%20rajpoot.com"
 console.log(url.replace('%20', '-'));
+console.log(url.includes('singh'));
+console.log(gamename.split('-'));  //spilt method converts string into array by passing separator and limit in it
