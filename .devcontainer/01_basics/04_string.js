bfa1995042a4059e5ffec46e178ad5f8299d12e8
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2025-06-09 04:19:26 +0000

    string methods

--- .devcontainer/01_basics/04_string.js
+++ .devcontainer/01_basics/04_string.js
@@ -3,5 +3,23 @@ let age= 23
 console.log('hello my name is $: {name} and my age is $: {23}');
 
 const gamename= new String('pungentshrine12')
+const gamename2=new String('online');
 console.log(gamename[0])
 console.log(gamename.__proto__);
+console.log(gamename.charAt(2));
+console.log(gamename.indexOf('s'));
+console.log(gamename.length);
+console.log(gamename.slice(6,9));
+console.log(gamename.toUpperCase());
+let user1="am"
+let user2="an"
+let user3="sin"
+let user4= 12
+let result= user1.concat("",user2,user3,user4)
+console.log(result);
+let num1= "  asma   "
+console.log(num1.trim());
+let farmerid= "44123544433221678"
+console.log(farmerid.substring(5,12))
+const url="https//amansingh%20rajpoot.com"
+console.log(url.replace('%20', '-'));
