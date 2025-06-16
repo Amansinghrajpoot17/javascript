e406216c2dab224fdf113fe3b167c1a9d80e9ad9
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2025-06-16 14:37:39 +0000

    time

--- .devcontainer/01_basics/06_date.js
+++ .devcontainer/01_basics/06_date.js
@@ -1,29 +1,29 @@
  let mydate= new Date()
-// console.log(mydate.toDateString()) // returns date
- console.log(mydate.toISOString())
-// console.log(mydate.toLocaleDateString()) // returns current date in mm/dd/yy 
-// console.log(mydate.toLocaleString())
-// console.log(mydate.toTimeString())
-// console.log(mydate.toLocaleTimeString())
-// console.log(typeof mydate) // interview questions type of date
+console.log(mydate.toDateString()) // returns date
+console.log(mydate.toISOString())
+console.log(mydate.toLocaleDateString()) // returns current date in mm/dd/yy 
+console.log(mydate.toLocaleString())
+ console.log(mydate.toTimeString())
+console.log(mydate.toLocaleTimeString())
+console.log(typeof mydate) // interview questions type of date
 
-// console.log(mydate.getDate())
-// console.log(mydate.getTime())
-//  let mycreateddate=new Date(2025,4,5)
-//  console.log(mycreateddate.toDateString())
+console.log(mydate.getDate())
+console.log(mydate.getTime())
+ let mycreateddate=new Date(2025,4,5)
+ console.log(mycreateddate.toDateString())
 
  let mytimestamp= Date.now //get the time in miliseconds
 
-//onsole.log(mytimestamp())
+console.log(mytimestamp())
 
  console.log(Math.floor(Date.now()/1000))// gets the time in milliseconds
 
 let newdate= new Date()
-//console.log(newdate.toLocaleTimeString("en-IN")) 
+ 
 
-//console.log(newdate.getSeconds())
-// console.log(newdate.getUTCHours())
-// console.log(newdate.getFullYear())
-// console.log(newdate.getMonth())
-// console.log(newdate.getDay())
-console.log(newdate.toLocaleTimeString("(IST = ( H / 360 ) * R)"))
+console.log(newdate.getSeconds())
+console.log(newdate.getUTCHours())
+console.log(newdate.getFullYear())
+console.log(newdate.getMonth())
+console.log(newdate.getDay())
+console.log(newdate.toLocaleString('ISN', {timeStyle:"full"}))
