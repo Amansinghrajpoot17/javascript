be8d6b541afceb87fbeb77f0f51c6e37c5b94b35
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2025-06-17 13:02:08 +0000

    array

--- .devcontainer/02_basics/02_array.js
+++ .devcontainer/02_basics/02_array.js
@@ -4,5 +4,9 @@ const myarr2= [4.8,9,7]
 //console.log(myarr)
 //const myarr3= myarr.concat(myarr2)
 //console.log(myarr3)
-const my= [...myarr, ...myarr2]
-console.log(my)
+// const my= [...myarr, ...myarr2]// spread method to join two array
+// console.log(my)
+// const another_array= [1.2,3,[4,5,6],7,8,[10,9,11]]
+// const real_another_array= another_array.flat(3)
+// console.log(real_another_array)
+console.log(Array.isArray("AMAN"))
