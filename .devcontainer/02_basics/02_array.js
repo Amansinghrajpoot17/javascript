60083ff0096a047cacfe57e29c7bc94535eff5c0
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2025-06-17 14:44:04 +0000

    array

--- .devcontainer/02_basics/02_array.js
+++ .devcontainer/02_basics/02_array.js
@@ -1,5 +1,8 @@
 const myarr= [2,2,3,4,5,6,7]
-const myarr2= [4.8,9,7]
+const array1= [4.8,9,7]
+let index=2;
+console.log(array1.at(0))
+
 //myarr.push(myarr2)
 //console.log(myarr)
 //const myarr3= myarr.concat(myarr2)
@@ -9,4 +12,9 @@ const myarr2= [4.8,9,7]
 // const another_array= [1.2,3,[4,5,6],7,8,[10,9,11]]
 // const real_another_array= another_array.flat(3)
 // console.log(real_another_array)
-console.log(Array.isArray("AMAN"))
+// console.log(Array.isArray("AMAN"))
+// console.log(Array.from({name:"AMAN"}))
+let score1= 100;
+let score2= 200;
+let score3= 300;
+// console.log(Array.of(score1,score2,score3))// creates a new array from a set of elements
