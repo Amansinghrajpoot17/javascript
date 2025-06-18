80b8228af802d3a7be96e9934fab58fc5c8e2fc5
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2025-06-18 15:03:08 +0000

    object

---
+++ .devcontainer/02_basics/04_objects.js
@@ -0,0 +1,23 @@
+const regularuser= {
+    email:"someone@gmail.com",
+    fullname:{
+        username:{
+            firstname:"Aman",
+            lastname:"rajpoot"
+        }
+    }
+}
+console.log(regularuser.fullname.username)
+const user=new Object()
+console.log(user)
+const obj1= {1:"a", 2:"b", 3:"c"}
+const obj2= {4:"d", 5:"e", 6:"f"}
+const obj3= Object.assign({}, obj1,obj2)
+console.log(obj3)
+const tinderuser={
+    userid:"113ab",
+    email:"a@gmail.com",
+    location:"INDORE",
+    Contactno:82134325
+}
+console.log(tinderuser)
