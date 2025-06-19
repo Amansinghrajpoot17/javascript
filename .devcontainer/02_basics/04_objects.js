2640356a3f0fe0fb7f39b2ba122419a625d4a80b
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2025-06-19 03:54:15 +0000

    object

--- .devcontainer/02_basics/04_objects.js
+++ .devcontainer/02_basics/04_objects.js
@@ -7,17 +7,31 @@ const regularuser= {
         }
     }
 }
-console.log(regularuser.fullname.username)
+//console.log(regularuser.fullname.username)
 const user=new Object()
-console.log(user)
+//console.log(user)
 const obj1= {1:"a", 2:"b", 3:"c"}
 const obj2= {4:"d", 5:"e", 6:"f"}
 const obj3= Object.assign({}, obj1,obj2)
-console.log(obj3)
+//console.log(obj3)
 const tinderuser={
     userid:"113ab",
     email:"a@gmail.com",
     location:"INDORE",
     Contactno:82134325
 }
-console.log(tinderuser)
+//console.log(tinderuser)
+//console.log(Object.keys(tinderuser))
+//console.log(Object.values(tinderuser))
+//console.log(Object.hasOwn(tinderuser,"userid")) // checks whether value exists or not
+const fuser={
+    ishuman:false,
+    printintroduction(){
+        console.log('my name is ${this.name}. am i human? ${this.ishuman}');
+    },
+};
+const bsuse= Object.create(fuser);
+bsuse.name="am";
+bsuse.ishuman=true;
+bsuse.printintroduction();
+console.log(bsuse)
