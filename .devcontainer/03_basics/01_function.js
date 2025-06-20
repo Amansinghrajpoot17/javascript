d8f46df7f5aad5c22b37b7dbe993eecf3682c368
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2025-06-20 14:58:01 +0000

    function

--- .devcontainer/03_basics/01_function.js
+++ .devcontainer/03_basics/01_function.js
@@ -6,3 +6,17 @@ function saymyname (){
     
 }
 console.log(saymyname())
+function add(number1,number2) {
+let result= number1+number2
+return result    
+}
+const result= add(5,7)
+console.log("RESULT IS:",result)
+function userloginmessage (_username){
+    if(_username===undefined){
+       console.log("please enter username") 
+        return
+    }
+    return `${_username} justloggedin`
+}
+console.log(userloginmessage())
