bd72db0a2e92833fe940e496abfb8e48c594a2b4
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2025-06-21 03:02:03 +0000

    function

--- .devcontainer/03_basics/01_function.js
+++ .devcontainer/03_basics/01_function.js
@@ -24,6 +24,7 @@ console.log(userloginmessage("Aman"))
 function calculateprice(...num1){
     return num1
 }
+// passing objects in a function
 console.log(calculateprice(2,3))
 const user={
     username:"sams",
@@ -33,3 +34,9 @@ function handleobject(anyobject){
     console.log(`usename is ${anyobject.username} and price is ${anyobject.price}`)
 }
 handleobject(user)
+// passing array in a function
+const myarray=[200,300,500]
+function returnsecondvalue (getnewarray){
+    return getnewarray[1]
+}
+console.log(returnsecondvalue(myarray))
