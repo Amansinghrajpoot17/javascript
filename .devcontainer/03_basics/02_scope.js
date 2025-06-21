17bc133531d568408b0d6f53fc9e977f7f87b734
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2025-06-21 03:39:46 +0000

    scope

---
+++ .devcontainer/03_basics/02_scope.js
@@ -0,0 +1,10 @@
+var c=90
+if(true){
+    let a=8
+    const b=9
+    var c=7
+}
+// console.log(a)
+// console.log(b)
+console.log(c)
+// value of var is accessed outside the scope that's why it cannot be hused in block code
