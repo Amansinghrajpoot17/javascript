125a553e0a93369089d2dd23951c1ea1ddf22f3b
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2025-06-21 12:47:55 +0000

    arrow

--- .devcontainer/03_basics/03_arrow.js
+++ .devcontainer/03_basics/03_arrow.js
@@ -1,12 +1,23 @@
-const { use } = require("react")
+//const { use } = require("react")
 
 const user = {
     username:"Aman",
      price:999,
      welcomemessage: function(){
-        console.log(`${this.username}, welcme to our platform`)
+      //   console.log(`${this.username}, welcme to our platform`)
+      //   console.log(this)
      }
 }
 user.welcomemessage()
 user.username="sam"
 user.welcomemessage()
+//console.log(this)
+// this refers to currrent context
+// in browser window is a global object  
+function cricket(){
+   const username= "Risabh Pant"
+   console.log(this.username)
+}
+cricket()
+// we cannot access this in the function.
+// this can only be used in the object or we can access it
