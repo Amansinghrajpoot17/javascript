58e73bd889268f4ce11f1ee5688b594a83c2fce1
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2025-06-22 02:54:08 +0000

    arrpw

--- .devcontainer/03_basics/03_arrow.js
+++ .devcontainer/03_basics/03_arrow.js
@@ -16,8 +16,23 @@ user.welcomemessage()
 // in browser window is a global object  
 function cricket(){
    const username= "Risabh Pant"
-   console.log(this.username)
+  // console.log(this.username)
 }
 cricket()
 // we cannot access this in the function.
 // this can only be used in the object or we can access it
+const chai = ()=> {
+   let username="aman"
+  // console.log(this)
+}
+chai()
+//()=>{} arrow function
+//const addtwo=(num1,num2)=>{
+  // return num1+ num2
+//}
+//console.log(addtwo(45,78))
+// implict return value
+const addtwo=(num1,num2) => num1+num2
+console.log(addtwo(23,78))
+const addtwo=(num1,num2) => ({username:"Aman"})
+console.log(addtwo(23,78))
