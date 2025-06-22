9e04ce9dba91791a5bba27b31c2b88cf0b4f4b94
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2025-06-22 16:26:05 +0000

    if statement

---
+++ .devcontainer/04_controlflow/one.js
@@ -0,0 +1,22 @@
+// if statement
+// if(condition){
+   // code
+// } //syntax of if statement
+
+// comparison operator:
+// <,>,<=,>=,==,===
+
+// if(2==="2"){
+//     console.log("values are equal")
+// }
+// console.log("values are not equal")
+const temp=45
+if(temp<50){
+    console.log("temperature is less than 50")
+}
+else{
+console.log("temp is greater than 50")
+}
+
+// if we are not using else statement then it will print both statement
+// if we are giving else statement then it will check condition and give output  
