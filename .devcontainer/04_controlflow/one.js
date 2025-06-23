fa269bd6e7d867d65930ccc3d8d6f768e83e67ed
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2025-06-23 03:34:01 +0000

    if-else

--- .devcontainer/04_controlflow/one.js
+++ .devcontainer/04_controlflow/one.js
@@ -19,4 +19,21 @@ console.log("temp is greater than 50")
 }
 
 // if we are not using else statement then it will print both statement
-// if we are giving else statement then it will check condition and give output  
+// if we are giving else statement then it will check condition and give output
+const score= 200
+if(score>100){
+    const power= "fly"
+    console.log(`user power is :${power}`)
+}  
+//console.log(`user power is :${power}`)
+const userloggrdin=true
+const debitcard=true
+if(userloggrdin&&debitcard){
+    console.log("allow to buy course")
+}
+const userlogged=true
+const userloggrdinfromgoggle=true
+const userloggedfromemail=false
+if(userloggedfromemail || userloggrdinfromgoggle ){
+    console.log("user logged in successfully")
+}
