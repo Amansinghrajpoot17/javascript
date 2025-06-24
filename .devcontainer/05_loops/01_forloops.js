2060cf73ce387a107e03c579ddc8eb6a8b488580
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2025-06-24 14:32:08 +0000

    for

--- .devcontainer/05_loops/01_forloops.js
+++ .devcontainer/05_loops/01_forloops.js
@@ -1,14 +1,27 @@
 for(let index= 0; index < 50; index++) {
     const element = index;
-    console.log(element)
+  //  console.log(element)
     
 }
-for (let i = 0; i < 10; i++) {
+for (let i = 5; i < 6; i++) {
     console.log(`outer loop value is: ${i}`)
-  for (let j = 0; j < 10; j++) {
-    console.log(`inner loop value is :${j} and inner loop value is: ${i}`)
+  for (let j = 1; j <= 10; j++) {
+   // console.log(`inner loop value is :${j} and inner loop value is: ${i}`)
+  // console.log(i +'*'+j +'='+i*j)
 
     
   }
     
+}
+let myarray= ["Aman","Abhinay","Akshay","Anuj","Anshul"]
+for (let index = 0; index < myarray.length; index++) {
+  const element = myarray[index];
+  console.log(element)
+}
+// break and continue
+for (let index = 1; index < 20; index++) {
+  if(index==5){
+    console.log
+  }
+  
 }
