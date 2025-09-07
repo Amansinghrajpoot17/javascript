287f00c9c6a2f7df4204cb3ee57cbdfba8cb7fbd
Amansinghrajpoot17 <aman401rajpoot@gmail.com>
2025-09-07 06:16:11 +0000

    update

--- .devcontainer/01_basics/01_variable.js
+++ .devcontainer/01_basics/01_variable.js
@@ -3,4 +3,4 @@ let accountname="Aman";
 var accountemail="aman120@gmail.com";
 accountcity="khurai";
  let accountstate;
-console.table([accountid,accountname,accountemail,accountcity,accountstate]);
+console.table([accountid,accountname,accountemail,accountcity,accountstate]);  
